349753556fa505ddca423c890538ca4790a6c054
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-06 15:05:30 +0200

    [test]: cycling random emails

--- js/script.js
+++ js/script.js
@@ -5,8 +5,11 @@ createApp({
 
   data() {
     return {
+      //3. Creo l'array vuoto che conterrà gli indirizzi email
       emails: [],
-      emailStr: ''
+      emailStr: '',
+      counter: 0,
+      emailsLimit: false
     }
   },
 
@@ -15,22 +18,53 @@ createApp({
       console.log('i metodi funzionano')
     },
 
-    //2. 
+    //2. Inizializzo un'istanza di Axios, dopo averlo importato dell'head
     getEmailAPI() {
+      
      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then((email) => {
-          console.log(email.data.response)
+
+          if (this.counter == 10) {
+            this.emailsLimit = true;
+          };
+
+          let emailStrTest = email.data.response;
+          this.emails.push(emailStrTest);
+          this.counter++
+
+
         })
         .catch((error) => {
-          console.log('L\'errore è :', error.code)
-        } )
 
-    }
+          console.log('L\'errore è :', error.code);
+
+        })
+
+    },
+
+    // 5. Ciclo gli elementi della lista con il mio array compilato
+    // generateEmails() {
+
+    //   const emailsLimit = 10;
+
+    //   // for(let email = 0; this.emails.length = emailsLimit; email++) {
+    //   //   this.getEmailAPI();
+
+    //   // }
+
+    // }
+
+
   },
 
   mounted() {
-    this.getConsole()
-    this.getEmailAPI()
+
+    this.getConsole();
+
+    this.getEmailAPI();
+
+  
+    console.log(this.emails);
 
   }
 
