ba493f1b7d98d3833092684c12a2d414d5ba10da
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-06 16:59:45 +0200

    [add]: print err msg in DOM; [edit] js formatting

--- js/script.js
+++ js/script.js
@@ -5,128 +5,49 @@ createApp({
 
   data() {
     return {
-      //3. Creo l'array vuoto che conterrà gli indirizzi email
+      // 3. Creo l'array vuoto che conterrà gli indirizzi email
       emails: [],
       emailStr: '',
       counter: 0,
-      counterEnd: 10,
-      emailsLimit: false,
-      testArray: []
+      errorMessage: ''
     }
   },
 
   methods: {
+
     getConsole() {
       console.log('i metodi funzionano')
     },
 
-    //2. Inizializzo un'istanza di Axios, dopo averlo importato dell'head
+    // 2. Inizializzo un'istanza di Axios, dopo averlo importato nell'head
     getEmailAPI() {
       
      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then((email) => {
 
-          
-          // let emailStrTest = email.data.response;
-
+          // 5. Compilo l'array con l'email generata dall'API
           this.emailStr = email.data.response
 
-          // this.emails.push(emailStrTest);
-          // this.generateEmails(emailStrTest);
-          this.emails.push(this.emailStr)
-
-          
+          this.emails.push(this.emailStr)       
 
         })
         .catch((error) => {
 
-          console.log('L\'errore è :', error.code);
+          this.errorMessage = error.code;
 
         })
 
     },
 
-    // 5. Ciclo gli elementi della lista con il mio array compilato
+    // 6. Ciclo dieci volte la generazione dell'email
     generateEmails() {
-
 
       while (this.counter < 10) {
         this.getEmailAPI();
         this.counter++
       }
 
-      // if (this.emailsLimit === false) {
-
-      //   console.log('io esisto');
-        
-      //   console.log(this.counter)
-      //   this.getEmailAPI();
-        
-      //   this.counter++;
-      //   console.log(this.counter)
-
-      // } else if (this.counter === this.counterEnd) {
-      //   this.emailsLimit === true
-      // }
-
-
-
-      // const emailsLimit = 10;
-
-      // this.emailStr = emailStrTest;
-
-      // for(this.counter = 0; this.counter === this.counterEnd; this.counter++) {
-      //   // this.getEmailAPI()
-      //   console.log(this.counter)
-
-
-      //   // if (!this.emails.includes(this.emailStr)) {
-      //   //   this.emails.push(this.emailStr);
-      //   //   this.counter++
-      //   // } else {
-      //   //   console.log('alfonso')
-      //   // }
-      //   // console.log(this.counter)
-
-
-      // }
     },
-
-    cyclingEmails() {
-
-      this.testArray.forEach(() => {
-        this.getEmailAPI()
-      })
-
-    },
-
-    // cyclingEmails() {
-    //   this.testArray.forEach(() => {
-    //     this.getEmailAPI()
-    //   })
-    // },
-
-    getEmailsLimit() {
-      axios.get('https://flynn.boolean.careers/exercises/api/array/integers?min=1&max=10&items=10')
-          .then(n => {
-            
-            this.testArray.push(n.data.response)
-            
-
-          })
-    },
-
-    testEmailGenerator() {
-
-      for(this.counter; this.counter == 10; this.counter++)
-        this.getEmailAPI()
-        
-        // if (this.counter == 10) {
-        //   this.getEmailAPI()
-        //   this.counter++
-        // }
-    }
-
 
   },
 
@@ -134,19 +55,8 @@ createApp({
 
     this.getConsole();
 
-    // this.getEmailAPI()
-
     this.generateEmails()
 
-
-    // this.testEmailGenerator();
-
-    // this.getEmailsLimit();
-
-    // this.cyclingEmails();
-    // console.log(this.testArray[0])
-
-  
     console.log(this.emails);
 
   }
