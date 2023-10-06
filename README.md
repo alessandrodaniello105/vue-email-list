4d5b618deafe14b3a37325c84a83477449edd9e1
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-06 14:07:46 +0200

    [edit]README;[add]axios + boolean random email API

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 Vue Email List
 ===
 
-1. Inizializzo un'istanza di Axios, dopo averlo importato dell'head
 1. Inizializzo un'istanza di createApp di Vue
+1. Inizializzo un'istanza di Axios, dopo averlo importato dell'head
 1. Creo l'array vuoto che conterrà gli indirizzi email
 1. Creo una lista nel DOM
 1. Ciclo gli elementi della lista con il mio array compilato
--- js/script.js
+++ js/script.js
@@ -1,22 +1,37 @@
-// 2. Inizializzo un'istanza di createApp di Vue
+// 1. Inizializzo un'istanza di createApp di Vue
 const { createApp } = Vue;
 
 createApp({
 
   data() {
     return {
-      emails: []
+      emails: [],
+      emailStr: ''
     }
   },
 
   methods: {
     getConsole() {
       console.log('i metodi funzionano')
+    },
+
+    //2. 
+    getEmailAPI() {
+     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((email) => {
+          console.log(email.data.response)
+        })
+        .catch((error) => {
+          console.log('L\'errore è :', error.code)
+        } )
+
     }
   },
 
   mounted() {
     this.getConsole()
+    this.getEmailAPI()
+
   }
 
 }).mount('#app')
